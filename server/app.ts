9b6ec6dd438d589998f41cae2fa751e0b018b76b
Noskov Evgenij <evg.noskov.noskov@gmail.com>
2021-05-23 00:14:15 +0300

    add eslint & prettier

--- server/app.ts
+++ server/app.ts
@@ -1,2 +1,14 @@
 import express from 'express'
+
 const app = express()
+
+async function start() {
+	app.listen(5000, () => {
+		console.log(`server work`)
+		console.log(`server workdd`)
+	})
+}
+
+start()
+
+//var test = "asdf";
--- server/dist/src/mod.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hi = void 0;
-exports.hi = 'trerrtw';
