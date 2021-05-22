d4dae11161a1f8a57c880e40dfce0c8aa857e820
Noskov Evgenij <evg.noskov.noskov@gmail.com>
2021-05-22 19:41:32 +0300

    create project structure

---
+++ server/dist/app.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const mod_1 = require("./src/mod");
+const app = express_1.default();
+console.log(mod_1.hi);
