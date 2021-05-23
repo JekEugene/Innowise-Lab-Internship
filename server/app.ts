8283e898e60adb84085d79d89786ee5567e5762c
Noskov Evgenij <evg.noskov.noskov@gmail.com>
2021-05-24 01:20:18 +0300

    add typeorm

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,11 @@
 import express from 'express'
+import { createConnection } from 'typeorm'
+
 import userController from './src/users/users.controller'
 import homeController from './src/home/home.controller'
 import authController from './src/auth/auth.controller'
 import videoController from './src/videos/videos.controller'
+
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -14,6 +17,7 @@ app.use(`/auth`, authController)
 app.use(`/video`, videoController)
 
 async function start() {
+	await createConnection()
 	app.listen(5000, () => {
 		console.log(`server work`)
 	})
