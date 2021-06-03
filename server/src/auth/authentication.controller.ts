03b4ac636c47352b5246dc2f5ce07567e774fd8f
Noskov Evgenij <evg.noskov.noskov@gmail.com>
2021-06-03 12:47:05 +0300

    change: login

--- server/src/auth/authentication.controller.ts
+++ server/src/auth/authentication.controller.ts
@@ -1,50 +1,56 @@
 import { Router, Request, Response } from "express"
 import { ICreateUserDto } from "../users/dto/create-user.dto"
-import { User } from "../users/users.model"
+import { IUserPayload } from "./user-payload.interface"
 const authController = Router()
-
 import { authenticationService } from './authentication.service'
-import { ITokens } from "./tokens.interface"
 
 authController.post(`/register`, async (req: Request, res: Response) => {
-	console.log(req.body)
 	const { login, password } = req.body
 	const user = await authenticationService.findUser(login)
 	if (user) {
 		return res.status(422).send(`user already exist`)
-	} else {
-		const hashPassword = await authenticationService.hashPassword(password)
-		const newUser: ICreateUserDto = {
-			login,
-			password: hashPassword
-		}
-		authenticationService.createUser(newUser)
-		return res.status(201).send(`user created`)
 	}
+	const hashPassword = await authenticationService.hashPassword(password)
+	const newUser: ICreateUserDto = {
+		login,
+		password: hashPassword
+	}
+	authenticationService.createUser(newUser)
+	return res.status(201).send(`user created`)
+	
 })
+
 authController.post(`/login`, async (req: Request, res: Response) => {
-	// const result = await authenticationService.login(req.body.login, req.body.password)
-	// if (result) {
-	// 	res.cookie(`accessToken`, result.accessToken, {
-	// 		maxAge: 1000 * 15,
-	// 		httpOnly: true,
-	// 	})
-	// 	res.cookie(`refreshToken`, result[1], {
-	// 		maxAge: 1000 * 60 * 60 * 24 * 7,
-	// 		httpOnly: true,
-	// 	})
-	// 	res.status(200)
-	// } else {
-	// 	res.status(401)
-	// }
-	// const { login, password } = req.body
-	// const user = await authenticationService.findUser(login)
-	// if (user) {
-	// 	const hashPassword = await authenticationService.hashPassword(password)
-	// 	const newUser: User = await authenticationService.createUser(login, hashPassword)
-	// } else {
-	// 	res.status(401).send(`wrong login or password`)
-	// }
+	const { login, password } = req.body
+	const user = await authenticationService.findUser(login)
+	if (!user) {
+		res.status(401).send(`user does not exist`)
+	}
+	const arePasswordsSame: boolean = await authenticationService.comparePasswords(user, password)
+	if (!arePasswordsSame) {
+		res.status(401).send(`wrong password`)
+	}
+
+	const userPayload: IUserPayload = {
+		id: user.id,
+		login: user.login
+	}
+
+	const refreshToken = authenticationService.signRefreshToken(userPayload)
+	authenticationService.createRefreshToken(user.id, refreshToken)
+	const accessToken = authenticationService.signAccessToken(userPayload)
+
+	res.cookie(`accessToken`, accessToken, {
+		maxAge: 1000 * 15,
+		httpOnly: true,
+	})
+	res.cookie(`refreshToken`, refreshToken, {
+		maxAge: 1000 * 60 * 60 * 24 * 7,
+		httpOnly: true,
+	})
+	res.cookie(`id`, user.id)
+	res.cookie(`login`, user.login)
+	res.status(200).send(`logged in`)
 })
 
 export = authController
--- server/src/auth/authentication.service.ts
+++ server/src/auth/authentication.service.ts
@@ -3,27 +3,10 @@ import * as jwt from 'jsonwebtoken'
 
 import { ICreateUserDto } from '../users/dto/create-user.dto'
 import { User } from '../users/users.model'
-import { ITokens } from './tokens.interface'
 import { Token } from './tokens.model'
 import { IUserPayload } from './user-payload.interface'
 
 export const authenticationService = {
-	// async register(createUserDto: ICreateUserDto): Promise<unknown> {
-	// 	const { login, password } = createUserDto
-	// 	const user = await User.findOne({ where: { login: createUserDto.login } })
-	// 	if (user) {
-	// 		return false
-	// 	}
-
-	// 	const hashPassword = await bcrypt.hash(password, 10)
-	// 	const newUser: ICreateUserDto = {
-	// 		login,
-	// 		password: hashPassword,
-	// 	}
-	// 	await User.create(newUser).save()
-
-	// 	return newUser
-	// },
 
 	async findUser(login: string): Promise<User> {
 		return await User.findOne({ where: { login } })
@@ -36,49 +19,30 @@ export const authenticationService = {
 	async createUser(user: ICreateUserDto): Promise<User> {
 		return await User.create(user).save()
 	},
+	
+	async comparePasswords(user: User, password: string): Promise<boolean> {
+		return await bcrypt.compare(
+			user.password,
+			password
+		)
+	},
 
+	async createRefreshToken(user_id: number, token: string): Promise<Token> {
+		return await Token.create({ token, user_id }).save()
+	},
 
-	async login(login: string, password: string): Promise<unknown> {
-		try {
-			const candidate = await User.findOne({ login })
-			if (candidate) {
-				const arePasswordsSame = await bcrypt.compare(
-					password,
-					candidate.password
-				)
-				if (arePasswordsSame) {
-					const user = Object.create(candidate)
-					
-					const userPayload: IUserPayload = {
-						id: user.id,
-						login: user.login
-					}
-
-					const accessToken: string = jwt.sign(
-						userPayload,
-						process.env.ACCESS_SECRET_TOKEN,
-						{ expiresIn: `10s` }
-					)
-					const refreshToken: string = jwt.sign(
-						userPayload,
-						process.env.REFRESH_SECRET_TOKEN,
-						{ expiresIn: `7d` }
-					)
-					Token.create({ token: refreshToken, user_id: user.id })
-
-					const tokens: ITokens = {
-						accessToken,
-						refreshToken
-					}
-					return tokens
-				} else {
-					return false
-				}
-			} else {
-				return false
-			}
-		} catch (e) {
-			console.log(e)
-		}
+	signAccessToken(userPayload: IUserPayload): string {
+		return jwt.sign(
+			userPayload,
+			process.env.ACCESS_SECRET_TOKEN,
+			{ expiresIn: `10s` }
+		)
+	},
+	signRefreshToken(userPayload: IUserPayload): string {
+		return jwt.sign(
+			userPayload,
+			process.env.REFRESH_SECRET_TOKEN,
+			{ expiresIn: `15d` }
+		)
 	},
 }
