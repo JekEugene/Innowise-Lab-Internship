d01d6571fae8d41a5394e11c4f95162d0e8485be
Noskov Evgenij <evg.noskov.noskov@gmail.com>
2021-06-05 14:10:03 +0300

    add: get user videos

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,9 +1,29 @@
-import { Router } from "express"
+import { Router, Request, Response } from "express"
+import { Video } from "../videos/videos.model"
+import { User } from "./users.model"
 const userController = Router()
 
 import { userService } from './users.service'
 
 
-userController.get(`/`, userService.userPage)
+userController.get(`/`, async (req: Request, res: Response) => {
+	const users: User[] = await userService.getAllUsers()
+	const sendUsers = users.map(user => {
+		return {id: user.id, login: user.login}
+	})
+	res.status(200).json(sendUsers)
+})
+
+userController.get(`/:id`, async (req: Request, res: Response) => {
+	const videos: Video[] = await userService.getUserVideos(res.locals.isAuth, res.locals.user.id, +req.params.id)
+	const sendVideos = videos.map(video => {
+		return { name: video.name, link: video.link, user_id: video.user_id }
+	})
+	res.status(200).json(sendVideos)
+})
+
+userController.post(`/newvideo`, async (req: Request, res: Response) => {
+	
+})
 
 export = userController
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,8 +1,24 @@
-import { Request, Response} from 'express'
+import { getRepository } from 'typeorm'
+import { Video } from '../videos/videos.model'
+import { User } from './users.model'
 
 export const userService = {
-	async userPage (req: Request, res: Response) :Promise<void> {
-		console.log(`hi`)
+	async getAllUsers() :Promise<User[]> {
+		return await User.find()
+	},
+	async getUserVideos(isAuth: boolean, userId: number, targetId: number): Promise<Video[]> {
+		return await getRepository(Video).createQueryBuilder(`video`)
+			.leftJoin(`video.permissions`, `permission`)
+			.where(`video.user_id = :targetId`, { targetId })
+			.andWhere(`video.type = 'READ_ALL'`)
+			.orWhere(`video.type = 'READ_AUTH' and :isAuth=true`, { isAuth })
+			.orWhere(`video.type = 'READ_CHOSEN' and permission.user_id = :userId and
+			permission.video_id = video.id`, { userId })
+			.orWhere(`video.type = 'READ_CHOSEN' and video.user_id = :userId`, { userId })
+			.orWhere(`video.type = 'READ_ADMIN' and permission.user_id = :userId and
+			permission.video_id = video.id and permission.type = 'ADMIN'`, { userId })
+			.orWhere(`video.type = 'READ_ADMIN' and video.user_id = :userId`, { userId })
+			.getMany()
 	}
 }
 
