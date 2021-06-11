afdd2e2a300d8dc391f8e05709c4570a94f500e4
Noskov Evgenij <evg.noskov.noskov@gmail.com>
2021-06-11 13:32:41 +0300

    add: authorization service

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,8 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 import * as jwt from 'jsonwebtoken'
 import { logger } from '../config/logger'
-import { tokenRepository } from '../modules/auth/token.repository'
-import { IUserPayload } from '../modules/auth/user-payload.interface'
+import { authorizationService } from '../modules/auth/authorization.service'
 
 export async function authUser(req: Request, res: Response, next: NextFunction): Promise<void> {
 	if (req.cookies?.accessToken) {
@@ -12,7 +11,7 @@ export async function authUser(req: Request, res: Response, next: NextFunction):
 			user = jwt.verify(token, process.env.ACCESS_SECRET_TOKEN)
 		} catch (err) {
 			logger.error(``, err)
-			await refreshToken(req, res)
+			await authorizationService.refreshToken(req, res)
 			return next()
 		}
 		res.locals.auth = true
@@ -24,60 +23,10 @@ export async function authUser(req: Request, res: Response, next: NextFunction):
 	}
 
 	if (req.cookies?.refreshToken) {
-		await refreshToken(req, res)
+		await authorizationService.refreshToken(req, res)
 		return next()
 	}
 	res.locals.auth = false
 	return next()
 }
 
-async function refreshToken(req: Request, res: Response): Promise<void> {
-	if (!req.cookies?.refreshToken) {
-		res.locals.auth = false
-		return
-	}
-	const token: string = req.cookies.refreshToken
-	let user
-	try {
-		user = jwt.verify(token, process.env.REFRESH_SECRET_TOKEN)
-	} catch (err) {
-		logger.error(``, err)
-		res.locals.auth = false
-		return
-	}
-	const userPayload: IUserPayload = {
-		id: user.id,
-		login: user.login,
-	}
-	const userToken = tokenRepository.getToken(userPayload.id, token)
-	if (!userToken) {
-		res.locals.auth = false
-		return
-	}
-	const accessToken: string = jwt.sign(
-		userPayload,
-		process.env.ACCESS_SECRET_TOKEN,
-		{ expiresIn: `10s` }
-	)
-	const refreshToken: string = jwt.sign(
-		userPayload,
-		process.env.REFRESH_SECRET_TOKEN,
-		{ expiresIn: `7d` }
-	)
-	tokenRepository.deleteToken(userPayload.id, token)
-	tokenRepository.createToken(userPayload.id, refreshToken)
-	res.cookie(`accessToken`, accessToken, {
-		maxAge: 1000 * 10,
-		httpOnly: true,
-	})
-	res.cookie(`refreshToken`, refreshToken, {
-		maxAge: 1000 * 60 * 60 * 24 * 7,
-		httpOnly: true,
-	})
-	res.cookie(`login`, user.login, { maxAge: 1000 * 60 * 60 * 24 * 7 })
-	res.cookie(`id`, user.id, { maxAge: 1000 * 60 * 60 * 24 * 7 })
-	res.locals.auth = true
-	res.locals.user = userPayload
-	res.locals.refreshToken = refreshToken
-	return
-}
---
+++ server/src/modules/auth/authorization.service.ts
@@ -0,0 +1,60 @@
+import { Request, Response } from 'express'
+import * as jwt from 'jsonwebtoken'
+import { logger } from '../../config/logger'
+import { tokenRepository } from './token.repository'
+import { IUserPayload } from './user-payload.interface'
+
+class AuthorizationService {
+	async refreshToken(req: Request, res: Response): Promise<void> {
+		if (!req.cookies?.refreshToken) {
+			res.locals.auth = false
+			return
+		}
+		const token: string = req.cookies.refreshToken
+		let user
+		try {
+			user = jwt.verify(token, process.env.REFRESH_SECRET_TOKEN)
+		} catch (err) {
+			logger.error(``, err)
+			res.locals.auth = false
+			return
+		}
+		const userPayload: IUserPayload = {
+			id: user.id,
+			login: user.login,
+		}
+		const userToken = tokenRepository.getToken(userPayload.id, token)
+		if (!userToken) {
+			res.locals.auth = false
+			return
+		}
+		const accessToken: string = jwt.sign(
+			userPayload,
+			process.env.ACCESS_SECRET_TOKEN,
+			{ expiresIn: `10s` }
+		)
+		const refreshToken: string = jwt.sign(
+			userPayload,
+			process.env.REFRESH_SECRET_TOKEN,
+			{ expiresIn: `7d` }
+		)
+		tokenRepository.deleteToken(userPayload.id, token)
+		tokenRepository.createToken(userPayload.id, refreshToken)
+		res.cookie(`accessToken`, accessToken, {
+			maxAge: 1000 * 10,
+			httpOnly: true,
+		})
+		res.cookie(`refreshToken`, refreshToken, {
+			maxAge: 1000 * 60 * 60 * 24 * 7,
+			httpOnly: true,
+		})
+		res.cookie(`login`, user.login, { maxAge: 1000 * 60 * 60 * 24 * 7 })
+		res.cookie(`id`, user.id, { maxAge: 1000 * 60 * 60 * 24 * 7 })
+		res.locals.auth = true
+		res.locals.user = userPayload
+		res.locals.refreshToken = refreshToken
+		return
+	}
+}
+
+export const authorizationService = new AuthorizationService()
